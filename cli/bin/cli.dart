3f72220665eb387ffcc11c9dd3174fb4ca2c3c17
OleJac18 <olejacobsen181997@gmail.com>
2026-08-30 12:40:24 +0200

    Handle errors

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,60 +1,17 @@
 import 'package:command_runner/command_runner.dart';
 
-const version = '0.0.1'; 
+const version = '0.0.1';
 
 void main(List<String> arguments) {
-  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+  var commandRunner = CommandRunner(
+    onError: (Object error) {
+      if (error is Error) {
+        throw error;
+      }
+      if (error is Exception) {
+        print(error);
+      }
+    },
+  )..addCommand(HelpCommand());
   commandRunner.run(arguments);
 }
-
-/*void main(List<String> arguments) {
-  if (arguments.isEmpty || arguments.first == 'help'){
-    printUsage();
-  } else if (arguments.first == 'version') {
-    print('Dartpedia CLI version $version');
-  } else if (arguments.first == 'wikipedia') {
-    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
-    searchWikipedia(inputArgs);
-  } else {
-    printUsage();
-  }
-}
-
-void searchWikipedia(List<String>? arguments) async {
-  final String articleTitle;
-
-  if(arguments == null || arguments.isEmpty) {
-    print('Please provide an article title.');
-    final inputFromStdin = stdin.readLineSync();
-    if (inputFromStdin == null || inputFromStdin.isEmpty) {
-      print('No article title provided. Exiting.');
-      return;
-    }
-    articleTitle = inputFromStdin;
-  } else {
-    articleTitle = arguments.join(' ');
-  }
-
-  print('Looking up articles about "$articleTitle". Please wait.');
-  
-  var articleContent = await getWikipediaArticle(articleTitle);
-  print(articleContent);
-}
-
-void printUsage() {
-  print("The following commands are valid: 'help', 'version, 'wikipedia <ARTICLE-TITLE>'");
-}
-
-Future<String> getWikipediaArticle(String articleTitle) async {
-  final url = Uri.https(
-  'en.wikipedia.org',
-  '/api/rest_v1/page/summary/$articleTitle  '
-  );
-  final response = await http.get(url);
-
-  if (response.statusCode == 200) {
-    return response.body;
-  } 
-
-  return 'Error: Unable to fetch article. Status code: ${response.statusCode}';
-}*/
