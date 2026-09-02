3fab19a1a91f352ea0acfd655b988849eb4785e1
OleJac18 <olejacobsen181997@gmail.com>
2026-09-02 19:00:48 +0200

    Improve HelpCommand

--- command_runner/lib/src/command_runner_base.dart
+++ command_runner/lib/src/command_runner_base.dart
@@ -7,12 +7,18 @@ import 'exceptions.dart'; // Add this line
 
 
 class CommandRunner {
-  CommandRunner({this.onError});
+  CommandRunner({this.onOutput,this.onError});
 
   final Map<String, Command> _commands = <String, Command>{};
 
   UnmodifiableSetView<Command> get commands =>
       UnmodifiableSetView<Command>(<Command>{..._commands.values});
+
+  /// If not null, this method is used to handle output. Useful if you want to
+  /// execute code before the output is printed to the console, or if you
+  /// want to do something other than print output the console.
+  /// If null, the onInput method will [print] the output.
+  FutureOr<void> Function(String)? onOutput;
 
   // Define the onError property.
   FutureOr<void> Function(Object)? onError;
@@ -22,7 +28,11 @@ class CommandRunner {
       final ArgResults results = parse(input);
       if (results.command != null) {
         Object? output = await results.command!.run(results);
-        print(output.toString());
+        if (onOutput != null) {
+          await onOutput!(output.toString());
+        } else {
+          print(output.toString());
+        }
       }
     } on Exception catch (exception) {
       if (onError != null) {
